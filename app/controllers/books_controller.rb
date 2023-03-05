250009528ce1c566d0988f907a9c0072963f5fdd
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-03-05 09:26:02 +0000

    test

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,17 +8,18 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    if@book.save
-      flash[:notice] = "Book was successfully updated."
+    if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
-      render :new
+      render :index
     end
   end
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
@@ -30,11 +31,15 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
+
 
   def destroy
     book = Book.find(params[:id])
@@ -46,6 +51,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.permit(:title, :body)
+    params.require(:book).permit(:title, :body)
   end
 end
