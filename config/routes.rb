b5cc482a9a993017d9818a9c879ebf85a8930d73
Saori Sakai <124588744+tyatewo@users.noreply.github.com>
2023-03-05 13:02:06 +0000

    完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get '/' => 'homes#top'
-  #resources :books
-  get 'books/new' =>'books#new'
-  post 'books' => 'books#create'
+  root to: 'homes#top'
   get 'books' => 'books#index'
+  post 'books' => 'books#create'
   get 'books/:id' => 'books#show', as: 'book'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  #resources :books
  end
